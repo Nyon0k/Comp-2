8e4d6b13615e25d8bd119b42033042d70c69d2a5
cristianvilela0K@gmail.com <cristianvilela0K@gmail.com>
2020-10-11 21:05:29 -0300

    #att Lab6

--- Intersecao/src/CalculadorIntersecaoEsperto.java
+++ Intersecao/src/CalculadorIntersecaoEsperto.java
@@ -5,7 +5,6 @@ public class CalculadorIntersecaoEsperto extends CalculadorIntersecao<Integer> {
     @Override
     public int getQuantidadeElementosEmComum(List<Integer> lista1, List<Integer> lista2) {
 
-        HashMap<Integer, Integer> l12 = new HashMap<>();
         HashSet<Integer> l1 = new HashSet<>(lista1);
         HashSet<Integer> inter = new HashSet<>();
 
